cd298dea7aa50073f3270cd20f897144cff2d381
Paolo Carta <ing.pcarta@gmail.com>
2020-11-07 19:21:50 +0100

    using custom key

--- instances.tf
+++ instances.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "jenkins_master" {
   provisioner "local-exec" {
     command = <<EOF
       aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id} \
-      && ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/install_jenkins.yaml
+      && ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' --private-key=~/.ssh/jenkins-aws-ssh-access ansible_templates/install_jenkins.yaml
     EOF
   }
   tags = {
@@ -59,28 +59,28 @@ resource "aws_instance" "jenkins_worker" {
   vpc_security_group_ids      = [aws_security_group.jenkins_workers_sg.id]
   subnet_id                   = aws_subnet.subnet_1_workers.id
 
-  # provisioner "remote-exec" {
-  #   when = destroy
-  #   inline = [
-  #     "java -jar /home/ec2-user/jenkins-cli.jar -auth @/home/ec2-user/jenkins_auth \
-  #     -s http://${aws_instance.jenkins_master.private_ip}:8080 -auth @/home/ec2-user/jenkins_auth delete-node ${self.private_ip}"
-  #   ]
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "ec2-user"
-  #     private_key = file("~/.ssh/jenkins-aws-ssh-access")
-  #     host        = self.public_ip
-  #   }
-  # }
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "java -jar /home/ec2-user/jenkins-cli.jar -auth @/home/ec2-user/jenkins_auth -s http://${self.tags.Master_Private_IP}:8080 -auth @/home/ec2-user/jenkins_auth delete-node ${self.private_ip}"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/jenkins-aws-ssh-access")
+      host        = self.public_ip
+    }
+  }
 
   provisioner "local-exec" {
     command = <<EOF
       aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id} \
-      && ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name} master_ip=${aws_instance.jenkins_master.private_ip}' ansible_templates/install_worker.yaml
+      && ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name} master_ip=${aws_instance.jenkins-master.private_ip}' --private-key=~/.ssh/jenkins-aws-ssh-access ansible_templates/install_worker.yaml
     EOF
   }
   tags = {
     Name = join("_", ["jenkins_worker_tf", count.index + 1])
+    Master_Private_IP = aws_instance.jenkins_master.private_ip
   }
   depends_on = [aws_main_route_table_association.set_worker_default_rt_assoc, aws_instance.jenkins_master]
 }
