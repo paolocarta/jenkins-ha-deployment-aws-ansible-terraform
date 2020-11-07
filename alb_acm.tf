a75f5675fa44411d8d48be78b68a3f45aa4e0021
Paolo Carta <ing.pcarta@gmail.com>
2020-11-07 19:21:07 +0100

    not using https without custom domain name

--- alb_acm.tf
+++ alb_acm.tf
@@ -1,22 +1,22 @@
 
-#ACM CONFIGURATION
-#Creates ACM issues certificate and requests validation via DNS(Route53)
-resource "aws_acm_certificate" "jenkins_lb_https_cert" {
-  provider          = aws.region-master
-  domain_name       = join(".", ["jenkins", data.aws_route53_zone.dns.name])
-  validation_method = "DNS"
-  tags = {
-    Name = "Jenkins-ACM"
-  }
-}
+# #ACM CONFIGURATION
+# #Creates ACM issues certificate and requests validation via DNS(Route53)
+# resource "aws_acm_certificate" "jenkins_lb_https_cert" {
+#   provider          = aws.region-master
+#   domain_name       = join(".", ["jenkins", data.aws_route53_zone.dns.name])
+#   validation_method = "DNS"
+#   tags = {
+#     Name = "Jenkins-ACM"
+#   }
+# }
 
-#Validates ACM issued certificate via Route53
-resource "aws_acm_certificate_validation" "cert" {
-  provider                = aws.region-master
-  certificate_arn         = aws_acm_certificate.jenkins_lb_https_cert.arn
-  for_each                = aws_route53_record.cert_validation
-  validation_record_fqdns = [aws_route53_record.cert_validation[each.key].fqdn]
-}
+# #Validates ACM issued certificate via Route53
+# resource "aws_acm_certificate_validation" "cert" {
+#   provider                = aws.region-master
+#   certificate_arn         = aws_acm_certificate.jenkins_lb_https_cert.arn
+#   for_each                = aws_route53_record.cert_validation
+#   validation_record_fqdns = [aws_route53_record.cert_validation[each.key].fqdn]
+# }
 
 ####ACM CONFIG END
 resource "aws_lb" "jenkins_master_app_loadbalancer" {
@@ -54,20 +54,20 @@ resource "aws_lb_target_group" "app-lb-tg" {
   }
 }
 
-resource "aws_lb_listener" "jenkins_listener_ssl" {
+# resource "aws_lb_listener" "jenkins_listener_ssl" {
 
-  provider          = aws.region-master
-  load_balancer_arn = aws_lb.jenkins_master_app_loadbalancer.arn
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.jenkins_lb_https_cert.arn
+#   provider          = aws.region-master
+#   load_balancer_arn = aws_lb.jenkins_master_app_loadbalancer.arn
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   certificate_arn   = aws_acm_certificate.jenkins_lb_https_cert.arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app-lb-tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app-lb-tg.arn
+#   }
+# }
 
 resource "aws_lb_listener" "jenkins_listener_http" {
 
@@ -77,13 +77,8 @@ resource "aws_lb_listener" "jenkins_listener_http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-    
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-lb-tg.arn
   }
 }
 
